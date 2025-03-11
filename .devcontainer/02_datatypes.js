37188105908d48cdd2c72d8d60efd4d173e1b583
Lavya <lavya075@gmail.com>
2025-03-11 22:34:45 +0000

    changes config

--- .devcontainer/02_datatypes.js
+++ .devcontainer/02_datatypes.js
@@ -1,1 +1,21 @@
+"use strict";//trat all JS code as newer version
 
+// alert("hello") // we are using nodejs ,not browser
+
+let name="Lavya"
+let age =19
+let isLoggedIn=true
+
+// number => 2 to power 53
+// bigint
+// string=>'' or ""
+// boolean => true/false
+//null => standalone value
+// undefined
+// symbol=> unique
+
+//object
+
+console.log(typeof "Hello"); //string
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
