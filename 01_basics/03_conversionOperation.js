1d06a70ade6167d969cd969f8827a7d0f91e0aad
Lavya <lavya075@gmail.com>
2025-03-11 22:59:02 +0000

    change done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score=33
+
+console.log(typeof score);
+let score2=undefined
+console.log(typeof(score2));
+
+let valueInNumber= Number(score2)
+console.log(typeof valueInNumber);
+
+
+//"33" =>33
+// "33abc"=> NaN
+//true=> 1;falsE=>0
+
+let isLoggedIn=null //false
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber= 33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
+
+
+
+
+
