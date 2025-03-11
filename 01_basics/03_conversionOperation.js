63edff6ceb3a8d031fa8e62772178dac2d2b920d
Lavya <lavya075@gmail.com>
2025-03-11 23:51:08 +0000

    config

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,24 +1,58 @@
-let score=33
+// let score=33
 
-console.log(typeof score);
-let score2=undefined
-console.log(typeof(score2));
+// console.log(typeof score);
+// let score2=undefined
+// console.log(typeof(score2));
 
-let valueInNumber= Number(score2)
-console.log(typeof valueInNumber);
+// let valueInNumber= Number(score2)
+// console.log(typeof valueInNumber);
 
 
-//"33" =>33
-// "33abc"=> NaN
-//true=> 1;falsE=>0
+// //"33" =>33
+// // "33abc"=> NaN
+// //true=> 1;falsE=>0
 
-let isLoggedIn=null //false
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn=null //false
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-let someNumber= 33
-let stringNumber=String(someNumber)
-console.log(typeof stringNumber);
+// let someNumber= 33
+// let stringNumber=String(someNumber)
+// console.log(typeof stringNumber);
+
+
+// //**********OPERATIONS**********//
+
+// let  value=3
+// let negValue=-value
+// console.log(negValue);
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2/3);
+// console.log(2%3);
+// console.log(2**3); //2 ki power 3
+
+let str1='Lavya'
+let str2= ' Jain'
+let str3=str1+str2
+console.log(str3);
+
+console.log(2+"3"); //overall string
+console.log("3"+"2");//string
+console.log(1+2+'2'); //3+'2'=32
+console.log('1'+2+2);// 122 number conversion
+//refer ECMA Script
+console.log(+true);
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
 
 
